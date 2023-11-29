c266f15126a409a998ba1fbd38cf4c1820dfd2fe
Yuriy Petrov <ypetrov@optum.com>
2023-11-29 10:11:02 -0700

    solved by camunda folks

--- src/test/java/com/optum/ibrcp/Test01.java
+++ src/test/java/com/optum/ibrcp/Test01.java
@@ -1,31 +1,31 @@
 package com.optum.ibrcp;
 
-import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests;
-import org.camunda.bpm.extension.junit5.test.ProcessEngineExtension;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.camunda.bpm.spring.boot.starter.test.helper.AbstractProcessEngineRuleTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 import java.util.Map;
 
-
-@ExtendWith({ProcessEngineExtension.class})
-@SpringJUnitConfig(locations = {"/camunda.cfg.xml"})
 @ActiveProfiles(profiles = {"test"})
-public class Test01 {
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class Test01 extends AbstractProcessEngineRuleTest {
 
-    ProcessEngine processEngine;
+    @Autowired
+    public RuntimeService rts;
 
     @Test
     @Deployment(resources = {"process.bpmn"})
     public void test01() {
-        RuntimeService rts = processEngine.getRuntimeService();
         Map<String, Object> vars = new HashMap<>();
 
         ProcessInstance procInst = rts.startProcessInstanceByKey("junit5-process", "key01", vars);
